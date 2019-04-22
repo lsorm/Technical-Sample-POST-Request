088a37c02828746bcbcf9bf45d61c2a1b11abe7a
Lilie <42131975+lsorm@users.noreply.github.com>
2019-04-22 12:05:47 -0700

    Update Technical Sample POST Request.py

--- Technical Sample POST Request.py
+++ Technical Sample POST Request.py
@@ -8,7 +8,7 @@ string_to_cut = "iamyourlyftdriver"
 result = ""
 
 for i in range(2, len(string_to_cut), 3) :
-    result += string_to_cut[i];
+    result += string_to_cut[i]
 
 datas = {"string_to_cut": string_to_cut, "return_string": result}
 
